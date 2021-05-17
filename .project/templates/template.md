ec8aed6c37cd4f32d4167907884be3549ebf8de3
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2021-05-16 21:31:14 -0600

    Move config file to config folder, add early templates

---
+++ .project/templates/template.md
@@ -0,0 +1,9 @@
+# Template Markdown File
+
+This file is a template, added to this folder by the Project utility.
+Fill it out with details about your new idea!
+
+## First Heading
+
+Ready, set, go!
+
---
+++ .project/templates/template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main(*args):
+    print("I'm a newly-minted Python program, created as a template by the Project utility!",
+          "Fill me in with your new idea!",
+          sep='\n')
+    print('And now for something completely different:\n')
+    import this
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
